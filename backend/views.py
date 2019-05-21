1cf4d36c9f4e7915662b057daa986b722326a06c
Muhammad Haviansyah <haviansyah09@gmail.com>
2019-05-21 13:19:55 +0700

    Mantap

--- backend/views.py
+++ backend/views.py
@@ -27,7 +27,7 @@ def user_logout(request):
 
 @login_required
 def condition(request,id):
-    response = requests.post('http://'+request.META['HTTP_HOST']+'/back/condition/',data={"id":id})
+    response = requests.post('https://'+request.META['HTTP_HOST']+'/back/condition/',data={"id":id})
     condition_data = response.json()
     id = condition_data["id"]
     title = condition_data["name"]
@@ -37,7 +37,7 @@ def condition(request,id):
 
 @login_required
 def unit(request,kondisi_id,unit_id):
-    response = requests.post('http://'+request.META['HTTP_HOST']+'/back/unit/',data={"unit_id":unit_id,"kondisi_id":kondisi_id})
+    response = requests.post('https://'+request.META['HTTP_HOST']+'/back/unit/',data={"unit_id":unit_id,"kondisi_id":kondisi_id})
     unit_data = response.json()
     title = Condition.objects.get(pk=kondisi_id).name + " " +unit_data["name"]
     reports = unit_data["report"]
@@ -46,7 +46,7 @@ def unit(request,kondisi_id,unit_id):
 
 @login_required
 def trend(request,id):
-    response = requests.post('http://'+request.META['HTTP_HOST']+'/back/trend/',data={"id":id})
+    response = requests.post('https://'+request.META['HTTP_HOST']+'/back/trend/',data={"id":id})
     eqt = Equipment.objects.get(pk=id)
     eqt_data = response.json()
     title = eqt.Condition.name + " | " + eqt.Unit.name
